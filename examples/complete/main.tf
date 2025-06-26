d516ce0206ab4eaaffd40ef060f9879e02923ee8
Wesley Kirkland <wesley@wesleyk.me>
2025-06-26 12:16:23 -0400

    Updating docs

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -5,8 +5,14 @@ provider "aws" {
 module "example" {
   source = "../.."
 
-  enabled_standards = var.enabled_standards
-  create_sns_topic  = var.create_sns_topic
-
-  context = module.this.context
+  control_finding_generator       = var.control_finding_generator #Must be set before delegation
+  create_sns_topic                = var.create_sns_topic
+  delegation_account_id           = "0123456789"
+  enable_default_standards        = false
+  enabled                         = var.enable_security_hub ? true : false
+  enabled_standards               = var.enabled_standards
+  environment                     = var.env
+  finding_aggregator_enabled      = false # Disabled due to delegation
+  finding_aggregator_linking_mode = "ALL_REGIONS"
+  namespace                       = var.namespace
 }
--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -14,3 +14,42 @@ variable "create_sns_topic" {
   type        = bool
   default     = false
 }
+
+variable "namespace" {
+  description = "Globally unique namespace, i.e. short company name. Used by Security Hub"
+  type        = string
+}
+
+variable "enable_security_hub" {
+  description = "Enable Security Hub"
+  type        = bool
+  default     = true
+}
+
+variable "control_finding_generator" {
+  description = <<-DOC
+  Updates whether the calling account has consolidated control findings turned on.
+
+  The possible values are:
+    - `SECURITY_CONTROL` - Security Hub generates a singles finding for a control check even when the check applies to multiple enabled standards
+    - `STANDARD_CONTROL` - Security Hub generates separate findings for a control check when the check applies to multiple enabled standards
+  DOC
+  type        = string
+  default     = "STANDARD_CONTROL"
+
+  validation {
+    condition     = contains(["SECURITY_CONTROL", "STANDARD_CONTROL"], var.control_finding_generator)
+    error_message = "The control_finding_generator value must be either 'SECURITY_CONTROL' or 'STANDARD_CONTROL'."
+  }
+}
+
+variable "env" {
+  description = "Env Name"
+  type        = string
+  default     = "prd"
+
+  validation {
+    condition     = contains(["sbx", "dev", "stg", "prd"], var.env)
+    error_message = "Invalid environment. Allowed values are: dev, qa, stg, prd."
+  }
+}
